73866b2da0747d51159f36289e6e0abecff9a3c6
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-02-27 11:45:32 -0300

    Update bubble-sort.rb

--- Algorithms/Sorting-Algorithms/bubble-sort.rb
+++ Algorithms/Sorting-Algorithms/bubble-sort.rb
@@ -1,14 +1,17 @@
 def bubble_sort(array)
     array_length = array.length
 
-    for current_iterarion in 0..array_length-1
+    for current_iteration in 0..array_length-1
         swapped = false
 
-        for index in 0..array_length-current_iterarion-2
+        for index in 0..array_length-current_iteration-2
 
             if array[index] > array[index+1]
                 array[index], array[index+1] = array[index+1], array[index]
                 swapped = true
+
+                puts "Passo #{current_iteration + 1}, Troca #{index + 1}: "
+                puts "#{array.join(', ')}"
             end
         end
 
@@ -16,3 +19,16 @@ def bubble_sort(array)
     end
 end
 
+if __FILE__ == $PROGRAM_NAME
+
+    puts "Digite os elementos do array separados por vírgula:"
+    user_input = gets.chomp
+    
+    inputed_array = user_input.split(',').map(&:to_i)
+
+    bubble_sort(inputed_array)
+
+    print "Array ordenado: "
+    inputed_array.each { |element| print "#{element} "}
+    puts
+end

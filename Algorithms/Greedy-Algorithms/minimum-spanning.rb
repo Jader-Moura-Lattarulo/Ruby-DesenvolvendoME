c250e630e063412aeb75122fa668c04963e041d5
Jader Moura Lattarulo <141791030+Jader-Moura-Lattarulo@users.noreply.github.com>
2024-02-28 17:01:40 -0300

    Update minimum-spanning.rb

--- Algorithms/Greedy-Algorithms/minimum-spanning.rb
+++ Algorithms/Greedy-Algorithms/minimum-spanning.rb
@@ -25,7 +25,60 @@ class Graph
 
             next if visited[vertex_2]
 
-            visited[]
+            visited[vertex_2] = true
+            result << [vertex_1, vertex_2, weight]
 
+            @graph[vertex_2].each {|vertex, weight| min_heap, insert([vertex_2, vertex, weight]) unless visited[vertex]}
+        end
+
+        result
+    end
+
+    def kruskal
+        result = []
+        edges = []
+
+        @graph.each do |vertex_1, neighbors|
+            neighbors.each{|vertex_2, weight| edges << [vertex_1, vertex_2, weight]}
+        end
+
+        edges = edges.sort_by{|edge| edge[2]}
+
+        parent = Array.new(@vertex) { |i| i }
+        rank = Array.new(@vertex, 0)
+
+        edges.each do |edge|
+            vertex_1, vertex_2, weight = edge
+            x = find(parent, vertex_1)
+            y = find(parent, vertex_2)
+
+            if x != y
+                result << [vertex_1, vertex_2, weight]
+                union(parent, rank, x, y)
+            end
+
+        end
+
+        result
+    end
+
+    private
+
+    def find(parent, i)
+        if parent[i] == i
+            i
+        else
+            find(parent,parent[i])
+        end
+    end
+
+    def union(parent, rank, x, y)
+        xroot = find(parent, x)
+        yroot = find(parent, y)
+
+        if rank[xroot] < rank[yroot]
+            parent[xroot] = yroot
+
+        elsif rank[xroot] > rank[yroot]
 
 end
